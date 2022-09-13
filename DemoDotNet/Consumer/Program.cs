b7a2b088b3c66ab0d5616c74c8c9faf15772687a
zballos <09edson@gmail.com>
2022-09-13 11:29:41 -0400

    Demo with producer and consumer using schema registry with .net

---
+++ DemoDotNet/Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig 
+{ 
+    GroupId = "test",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, demo.kafka.Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<demo.kafka.Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
+
+consumer.Subscribe("courses");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Message {result.Message.Value.descricao}");
+}
---
+++ DemoDotNet/Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, demo.kafka.Curso>(config)
+    .SetValueSerializer(new AvroSerializer<demo.kafka.Curso>(schemaRegistry))
+    .Build();
+
+var message = new Message<string, demo.kafka.Curso>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new demo.kafka.Curso
+    {
+        id = Guid.NewGuid().ToString(),
+        descricao = $"Lambdas AWS Global Event: {Guid.NewGuid().ToString()}"
+    } 
+};
+
+var result = await producer.ProduceAsync("courses", message);
+
+Console.WriteLine($"{result.Offset}");
